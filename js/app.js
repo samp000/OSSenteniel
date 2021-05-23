fc6826d0285db78599c6a1bc377b15e03b520ee1
samp000 <sandeshdmarathe333@gmail.com>
2021-05-23 13:20:33 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,121 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar_menu");
+const navLogo = document.querySelector("#navbar_logo");
+
+//display mobile menu
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+  $("body").css("overflow", "hidden");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// hide menu bar while clicking on nav links
+
+const hideMobileMenu = () => {
+  $("body").css("overflow", "scroll");
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 968 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
+
+// Navbar hiding
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.boxShadow =
+      "0 1px 1px  rgb(246, 246, 246)";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+    document.getElementById("navbar").style.borderBottomColor = "transperent";
+    document.getElementById("navbar").style.boxShadow = "none";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// slideshow
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 4 seconds
+}
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
+
+// loader
+
+var loader = document.querySelector(".loader_container");
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disappear");
+}
+
+// accordium(about us)
+
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
+
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
+
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, tbname) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tbname).style.display = "block";
+  evt.currentTarget.className += " active";
+}
